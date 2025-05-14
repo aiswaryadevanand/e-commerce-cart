a17baad830de7e0107e108fd0a22cee8a6c42511
aiswaryadevanand <aiswaryaanand512@gmail.com>
2025-05-14 13:13:54 +0530

    2nd

--- src/component/cart/cart.js
+++ src/component/cart/cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeFromCart } from "../../slices/cartSlices";
+import './cart.css'
 
 
 function Cart() {
@@ -10,32 +11,38 @@ function Cart() {
   const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
-    <div>
-      <h2>Your Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>No items in cart.</p>
-      ) : (
-        <table>
-        
-          {cartItems.map((item, index) => (
-            <tr key={index}>
-            
-              <td><img src={item.image} alt={item.name} width="50" /></td>
-              <td>{item.name} - ₹{item.price} x {item.quantity}</td>
-              <td><button
+    <div className="cart-container">
+  <h2>Your Cart</h2>
+  {cartItems.length === 0 ? (
+    <p>No items in cart.</p>
+  ) : (
+    <table className="cart-table">
+      
+      <tbody>
+        {cartItems.map((item, index) => (
+          <tr key={index}>
+            <td>
+              <img src={item.image} alt={item.name} className="cart-img" />
+            </td>
+            <td>
+              {item.name} - ₹{item.price} x {item.quantity}
+            </td>
+            <td>
+              <button
                 onClick={() => dispatch(removeFromCart(index))}
-                style={{ marginLeft: 10, color: "red" }}
+                className="remove-btn"
               >
                 Remove
-              </button></td>
-              </tr>
-            
-          ))}
-        
-        </table>
-      )}
-      <h3>Total: ₹{total.toFixed(2)}</h3>
-    </div>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )}
+  <h3 className="total-price">Total: ₹{total.toFixed(2)}</h3>
+</div>
+
   );
 }
 
